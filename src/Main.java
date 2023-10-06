0e3fa9456a31b191421025ceffcc657e5564cab2
Ricardo Soares <soaresricaventura@gmail.com>
2023-10-06 10:55:38 +0100

    Class "Main" - Menu

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -12,26 +12,68 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        ParkingLot park = new ParkingLot();
 
+        ArrayList<Student> studentList = new ArrayList<Student>(1000);
+        ParkingLot park = new ParkingLot();
+        Scanner input =new Scanner(System.in);
+
+        int op = -1;
+
+        while(op != 0){
             System.out.println("Choose one option: ");
             System.out.println("1. Add Ticket ");
             System.out.println("2. Remove Ticket ");
             System.out.println("3. View Ticket ");
+            System.out.println("4. Add Student");
+            System.out.println("5. Remove Student");
+            System.out.println("6. View Student");
             System.out.println("0. Leave Program ");
             System.out.println("Option: ");
+            op =input.nextInt();
+            input.nextLine();
 
-
-
+            if(op < 0 || op > 4){
                 System.out.println("Choose one option: ");
                 System.out.println("1. Add Ticket ");
                 System.out.println("2. Remove Ticket ");
                 System.out.println("3. View Ticket ");
-
+                System.out.println("4. Add Student");
+                System.out.println("5. Remove Student");
+                System.out.println("6. View Student");
+                System.out.println("0. Leave Program ");
+                System.out.println("Option: ");
+                op =input.nextInt();
+                input.nextLine();
+            }
+            switch (op){
                 case 1:
                     System.out.println("Ola Manuel");
                     break;
                 case 2:
+                    System.out.println("Ola Tiago");
+                    break;
+                case 3:
+                    System.out.println("Ola Jorge");
+                    break;
+                case 4:
+                    System.out.println("What is the Student Number ?");
+                    int id = input.nextInt();
+                    System.out.println("What is the student name ?");
+                    String name = input.nextLine();
+                    System.out.println("What is the student course ?");
+                    String course = input.nextLine();
+                    Student novo = new Student(id,name,course);
+                    studentList.add(novo);
+                    System.out.println(novo.toString());
+                    break;
+                case 5:
+                    System.out.println("What is the Student Number ?");
+                    int idS = input.nextInt();
+                    /*removeS(idS,studentList);*/
+                    break;
+
+            }
+        }
 
     }
 }
