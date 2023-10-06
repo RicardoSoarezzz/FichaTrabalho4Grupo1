505425f445f39e6f86ccefa85240ed7a3ba53631
Ricardo Soares <soaresricaventura@gmail.com>
2023-10-06 09:54:51 +0100

    Class "Main" - Menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        ParkingLot park = new ParkingLot();
+        Scanner input = new Scanner(System.in);
 
+        int op = -1;
+
+        while (op != 0) {
+            System.out.println("Choose one option: ");
+            System.out.println("1. Add Ticket ");
+            System.out.println("2. Remove Ticket ");
+            System.out.println("3. View Ticket ");
+            System.out.println("0. Leave Program ");
+            System.out.println("Option: ");
+            op = input.nextInt();
+            input.nextLine();
+
+            if (op < 0 || op > 4) {
+                System.out.println("Choose one option: ");
+                System.out.println("1. Add Ticket ");
+                System.out.println("2. Remove Ticket ");
+                System.out.println("3. View Ticket ");
+                System.out.println("0. Leave Program ");
+                System.out.println("Option: ");
+                op = input.nextInt();
+                input.nextLine();
+            }
+            switch (op) {
+                case 1:
+                    System.out.println("Ola Manuel");
+                    break;
+                case 2:
+                    System.out.println("Ola Joao");
+                    break;
+                case 3:
+                    System.out.println("Ola Tiago");
+                    break;
+            }
+        }
     }
 }
