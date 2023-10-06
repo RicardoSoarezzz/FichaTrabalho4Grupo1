a00ec5e2e7edd52be88393209d84703d92194dfe
Ricardo Soares <105425605+RicardoSoarezzz@users.noreply.github.com>
2023-10-06 10:48:38 +0100

    Tiago b (#10)
    * Tiago
    
    * Ticket and Student
    
    * Ticket v2
    
    * Class variables updated
    
    * Menu
    
    * Menu v2
    
    * Menu vStudentList
    
    * Menu vF
    
    ---------
    
    Co-authored-by: Tbrandao23 <tiagoleal76@gmail.com>
    Co-authored-by: jorgefdc <jorgefdc@outlook.com>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public void removeS(int id,ArrayList<Student> sl){
+        for(int i = 0; i < sl.size();i++){
+            if(id == sl.get(i).getId()){
+                sl.remove(i);
+            }
+        }
+    }
     public static void main(String[] args) {
         ParkingLot park = new ParkingLot();
 
@@ -14,27 +22,16 @@ public class Main {
             System.out.println("Option: ");
 
 
-            if (op < 0 || op > 4) {
+
                 System.out.println("Choose one option: ");
                 System.out.println("1. Add Ticket ");
                 System.out.println("2. Remove Ticket ");
                 System.out.println("3. View Ticket ");
-                System.out.println("0. Leave Program ");
-                System.out.println("Option: ");
-                op = input.nextInt();
-                input.nextLine();
-            }
-            switch (op) {
+
                 case 1:
                     System.out.println("Ola Manuel");
                     break;
                 case 2:
-                    System.out.println("Ola Joao");
-                    break;
-                case 3:
-                    System.out.println("Ola Tiago");
-                    break;
-            }
-        }
+
     }
 }
